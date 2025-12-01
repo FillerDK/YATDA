508af6ec989471fd30a4ae14a158cdc83973caf1
FillerDK <63036804+FillerDK@users.noreply.github.com>
2025-12-01 14:21:43 +0100

    Update Reminder.swift

--- YATDA/YATDA/Model/Reminder.swift
+++ YATDA/YATDA/Model/Reminder.swift
@@ -1,12 +1,3 @@
-// Reminder.swift
-// Project: YATDA
-// Compiled with Swift version 6.0
-//
-// Created by ksd/Kaj Schermer Didriksen on 29/10/2025 at 11.24.
-// Copyright © 2025 ksd. All rights reserved.
-//
-//
-
 import Foundation
 import FirebaseFirestore
 
