5c03e0dcbd61108984ddb4e1387a058f25fc0a68
FillerDK <63036804+FillerDK@users.noreply.github.com>
2025-12-01 14:21:10 +0100

    Clean up header comments in AddReminderView.swift
    Removed header comments from AddReminderView.swift

--- YATDA/YATDA/Views/Sheets/AddReminderView.swift
+++ YATDA/YATDA/Views/Sheets/AddReminderView.swift
@@ -1,12 +1,3 @@
-// AddReminderView.swift
-// Project: YATDA 
-// Compiled with Swift version 6.0
-//
-// Created by ksd/Kaj Schermer Didriksen on 29/10/2025 at 11.27.
-// Copyright © 2025 ksd. All rights reserved.
-//
-// 
-
 import SwiftUI
 
 struct AddReminderView: View {
